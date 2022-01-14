d2d1053c8132198917e4924eeaa62c3402672dec
RiggityRussell <77250752+RiggityRussell@users.noreply.github.com>
2022-01-13 21:00:49 -0500

    Initial Commit

---
+++ Lesson1/numberFun.py
@@ -0,0 +1,24 @@
+import random
+
+var1 = random.randrange(1,100)
+var2 = random.randrange(1,100)
+
+print(f"First number = {var1}")
+print(f"Second number = {var2}")
+print(f"{var1} / {var2} = {var1 / var2}")
+print(f"{var1} // {var2} = {var1 // var2}")
+print(f"{var1} % {var2} = {var1 % var2}")
+print(f"{var1} + {var2} = {var1 + var2}")
+print(f"{var1} - {var2} = {var1 - var2}")
+print(f"{var1} * {var2} = {var1 * var2}")
+
+NUMBER25 = 25
+var2 = NUMBER25
+print(f"First number = {var1}")
+print(f"Second number = {var2}")
+print(f"{var1} / {var2} = {var1 / var2}")
+print(f"{var1} // {var2} = {var1 // var2}")
+print(f"{var1} % {var2} = {var1 % var2}")
+print(f"{var1} + {var2} = {var1 + var2}")
+print(f"{var1} - {var2} = {var1 - var2}")
+print(f"{var1} * {var2} = {var1 * var2}")
---
+++ Lesson1/stringFun.py
@@ -0,0 +1,36 @@
+# exercise 1 - Russell Arlt
+print("---------------------------------------")
+print("Exercise 1")
+name = "russell arlt"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(f"My first initial is: {name[0].upper()}")
+
+#exercise 2 - Russell Arlt
+print("---------------------------------------")
+print("Exercise 2")
+noun = "Kitten"
+adj = "sleepy"
+verb = "exploded"
+ending = "the minds of everyone on the internet."
+sentence1 = "The " + adj + " purring " + noun + " " + verb + " " + ending
+sentence2 = f"The {adj} purring {noun} {verb} {ending}"
+print(sentence1.upper())
+print(sentence2.lower())
+
+#exercise 3 - Russell Arlt
+print("--------------------------------------")
+print("Exercise 3")
+longExerpt = """Shadow had done three years in prison.
+\tHe was big enough, and looked don't-mess-with-me enough
+that his biggest problem was killing time.
+\tSo he kept himself in shape, and taught himself coin tricks,
+and thought a lot about how much he loved his wife.
+"""
+print(longExerpt)
+
+#exercise 4 -Russell Arlt
+print("-------------------------------------")
+print("Exercise 4")
+print("Hello\tLisa\nI\tdidn't\nrealize\tthis\nclass\tstarted\nwith\tPython!")
