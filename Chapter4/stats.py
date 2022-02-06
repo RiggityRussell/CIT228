0cfbc313b59b162ee245aba143576b138e2c8ce6
RiggityRussell <77250752+RiggityRussell@users.noreply.github.com>
2022-02-06 08:41:30 -0500

    resubmit_stats.py

--- Chapter4/stats.py
+++ Chapter4/stats.py
@@ -6,13 +6,14 @@ ran_num = random.randrange(10,100)
 print(f"Random number chosen is {ran_num}")
 num_list = [value for value in range(0,ran_num + 1)]
 print(num_list)
-print(f"The largest number {num_list[-1]}")
-print(f"The smallest number {num_list[0]}")
+print(f"The largest number {max(num_list)}")
+print(f"The smallest number {min(num_list)}")
 x_num = 0
 for x in num_list:
     x_num = x_num + x
 print(f"The total of all the numbers = {x_num}")
-avg_num = x_num / num_list[-1]
+
+avg_num = x_num / max(num_list)
 print(f"The average number = {avg_num}")
 
 
