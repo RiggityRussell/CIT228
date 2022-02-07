3d9d7822bdcc77b54b7c945ebd9185459ae1926f
RiggityRussell <77250752+RiggityRussell@users.noreply.github.com>
2022-02-07 15:40:53 -0500

    reupload sum

--- Chapter4/stats.py
+++ Chapter4/stats.py
@@ -8,12 +8,8 @@ num_list = [value for value in range(0,ran_num + 1)]
 print(num_list)
 print(f"The largest number {max(num_list)}")
 print(f"The smallest number {min(num_list)}")
-x_num = 0
-for x in num_list:
-    x_num = x_num + x
-print(f"The total of all the numbers = {x_num}")
-
-avg_num = x_num / max(num_list)
+print(f"The total of all the numbers = {sum(num_list)}")
+avg_num = sum(num_list) / max(num_list)
 print(f"The average number = {avg_num}")
 
 
