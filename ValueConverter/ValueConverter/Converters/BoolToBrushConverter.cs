a31497b135ee47c857fde4244a218068667b5c0d
RiggityRussell <77250752+RiggityRussell@users.noreply.github.com>
2022-03-16 18:46:02 -0400

    Add files via upload

---
+++ ValueConverter/ValueConverter/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ValueConverter.Converters
+{
+    public class BoolToBrushConverter : IValueConverter //CTRL PERIOD WILL USE WHATEVER THING NEEDED
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isOnline = (bool)value;
+            if (isOnline)
+                return Brushes.Green;
+            else
+                return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ValueConverter/ValueConverter/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ValueConverter.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter // added System.Windows.Data
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) //We care about the object value.
+        {
+            var booleanVal = (bool)value; //Converts whatever we pass to a boolean
+            if (booleanVal)
+                return Visibility.Visible; // added System.Windows
+            else
+                return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
